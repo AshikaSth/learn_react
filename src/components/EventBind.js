8abfc7b8513cb8c995f17d0888294da371bb4c65
Ashika <ashikashrestha114@gmail.com>
2025-06-05 23:08:56 +0545

    binding in the class constructor

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -7,6 +7,8 @@ class EventBind extends Component {
     this.state = {
        message: 'Hello'
     }
+    this.clickHandler = this.clickHandler.bind(this)
+  //binding the clickHandler method to the class instance
   }
  
 
@@ -23,7 +25,8 @@ class EventBind extends Component {
       <div>
         <div>{this.state.message}</div>
          {/* <button onClick={this.clickHandler.bind(this)}>Click</button>  */}
-         <button onClick={()=> this.clickHandler()}>Click</button>
+         {/* <button onClick={()=> this.clickHandler()}>Click</button> */}
+         <button onClick={this.clickHandler}>Click</button>
         
       </div>
     )
