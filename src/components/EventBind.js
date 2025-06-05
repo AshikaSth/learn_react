a082691fd81df6eb7401dbe712a189d4888a9f94
Ashika <ashikashrestha114@gmail.com>
2025-06-05 23:00:30 +0545

    binding using bind keyword

---
+++ src/components/EventBind.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       message: 'Hello'
+    }
+  }
+  // clickHandler  ()=> {
+  //   this.setState({
+  //     message:'Goodbye'
+  //   })
+  //   console.log(this)
+  //   //this is undefined here 
+  // }
+
+  clickHandler ()  {
+    this.setState({
+      message:'Goodbye'
+    })
+    console.log(this)
+    //this is undefined here 
+  }
+  
+  render() {
+    return (
+      <div>
+        <div>{this.state.message}</div>
+        <button onClick={this.clickHandler.bind(this)}>Click</button>
+        
+      </div>
+    )
+  }
+}
+
+export default EventBind
